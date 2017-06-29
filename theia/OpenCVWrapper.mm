2e9eeb95f012d71947177e9cf6c17bc0124784c2
Bhavin <bhavingpt@gmail.com>
2017-06-28 20:13:48 -0700

    i dont know wtf happened, dlib is working

--- theia/OpenCVWrapper.mm
+++ theia/OpenCVWrapper.mm
@@ -9,9 +9,12 @@
 #import <opencv2/opencv.hpp>
 #import "OpenCVWrapper.h"
 
+#import <dlib/image_processing.h>
+#import <dlib/image_io.h>
+
 using namespace std;
 using namespace cv;
-
+using namespace dlib;
 
 /// Converts an NSImage to Mat.
 static void NSImageToMat(NSImage *image, cv::Mat &mat) {
